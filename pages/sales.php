2363e3beddbf7c20903b47e5014b9e99983eb471
milanjethva-09 <milanjethva@gmail.com>
2025-06-28 13:13:58 +0530

    Add sales.php and inventory.php feature pages using shared layout

---
+++ pages/sales.php
@@ -0,0 +1,7 @@
+<?php require '../includes/header.php'; ?>
+<?php require '../includes/sidebar.php'; ?>
+<div class="container-fluid py-4">
+  <h2>Sales Module</h2>
+  <p>Feature coming soon...</p>
+</div>
+<?php require '../includes/footer.php'; ?>
